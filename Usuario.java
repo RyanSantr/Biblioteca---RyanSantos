6c7911c901c8ae4c3925349df513b79bb1f9c1d5
Ryan Santos <123828897+RyanSantr@users.noreply.github.com>
2026-03-28 15:31:21 -0300

    Refactor Usuario class to use LocalDate

--- Usuario.java
+++ Usuario.java
@@ -1,38 +1,61 @@
 package entidade;
 
-import java.util.Date;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class Usuario {
 	  private String nome;
 	  private String registro;
-	  private Date dataNasc;
+	  private LocalDate dataNasc;
+	  
+	  public Usuario() {
+		  
+	  }
+	  
+	  public Usuario(String nome, String registro, LocalDate dataNasc) {
+			
+			this.nome = nome;
+			this.registro = registro;
+			this.dataNasc = dataNasc;
+			
+		}
+	  
+	  
 	  
 	  public String getNome() {
 		  return nome;
 	  }
-	  public Usuario(String nome, String registro, Date dataNasc) {
-		super();
-		this.nome = nome;
-		this.registro = registro;
-		this.dataNasc = dataNasc;
-	}
+	  
 	  public void setNome(String nome) {
 		  this.nome = nome;
 	  }
+	  
 	  public String getRegistro() {
 		  return registro;
 	  }
+	  
 	  public void setRegistro(String registro) {
 		  this.registro = registro;
 	  }
-	  public Date getDataNasc() {
+	  
+	  public LocalDate getDataNasc() {
 		  return dataNasc;
 	  }
-	  public void setDataNasc(Date dataNasc) {
+	  
+	  public void setDataNasc(LocalDate dataNasc) {
 		  this.dataNasc = dataNasc;
 	  }
-
 	  
-	
+	  @Override
+	  public String toString() {
+			 DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		  String dataformatada = dataNasc != null ? dataNasc.format(formatter) : "nao informada"; 
+		  
+		  
+		  return "Nome: " + nome + "\n" + "Registro: " + registro + "\n"+ "Data Nascimento: " + dataformatada;
+	  }
+	  
+	  
 	
 }
