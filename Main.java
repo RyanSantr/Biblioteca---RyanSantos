b6bd1ddbc283cf9c637e9e46af93b8b99ea86470
Ryan Santos <123828897+RyanSantr@users.noreply.github.com>
2026-03-28 15:30:51 -0300

    Refactor Main class for better structure
    Reorganize user registration and book addition logic.

--- Main.java
+++ Main.java
@@ -11,29 +11,13 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
 		
+		// cadadastro livro
 		Biblioteca biblioteca = new Biblioteca();
 		Livro livro1 = new Livro();
 		Livro livro2 = new Livro();
 		Livro livro3 = new Livro();
-		
-		Scanner sc = new Scanner(System.in);
-		
-		
-		Usuario user1 = new Usuario();
-		
-		System.out.println("Cadastro");
-		System.out.println("Usuario digite seu nome: ");
-		user1.setNome(sc.nextLine());
-		
-		System.out.println("Digite seu Registro");
-		user1.setRegistro(sc.nextLine());
-		
-		System.out.println("Digite sua Data de Nascimento");
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-		LocalDate date = LocalDate.parse(sc.nextLine(), formatter);
-		// todos os dados
-	
 		
 		livro1.setTitulo("O Morro dos Ventos Uivantes");
 		livro1.setAutor("Emily Brontë");
@@ -42,7 +26,6 @@ public class Main {
 		livro2.setTitulo("O Senhor dos Aneis");
 		livro2.setAutor("J.R.R. Tolkien");
 		livro2.setQtdDisponivel(6);
-		
 		livro3.setTitulo("Iracema");
 		livro3.setAutor("José de Alencar");
 		livro3.setQtdDisponivel(2);
@@ -52,7 +35,31 @@ public class Main {
 		biblioteca.addLivro(livro3);
 		
 		biblioteca.listarLivros();
+
 		
+		// Castrp de Usuario;
+		
+		Usuario user1 = new Usuario();
+		System.out.println("Cadastro");
+		System.out.println("Usuario digite seu nome: ");
+		user1.setNome(sc.nextLine());
+		
+		System.out.println("Digite seu Registro");
+		user1.setRegistro(sc.nextLine());
+		
+		System.out.println("Digite sua Data de Nascimento (dd/MM/yyyy");
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		String datasc = sc.nextLine();
+		LocalDate dataNasc = LocalDate.parse(datasc, formatter);
+		user1.setDataNasc(dataNasc);
+		
+		biblioteca.AddUsuario(user1);
+		biblioteca.ListarUsuarioss();
+		
+		// todos os dados
+	
+
+		sc.close();
 		
 		
 	}
