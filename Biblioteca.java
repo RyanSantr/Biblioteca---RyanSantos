ca019a6571593cd15af8d3101c8659de6ff575cf
Ryan Santos <123828897+RyanSantr@users.noreply.github.com>
2026-03-28 15:31:48 -0300

    Add user management methods to Biblioteca class

--- Biblioteca.java
+++ Biblioteca.java
@@ -33,7 +33,28 @@ public class Biblioteca {
 				System.out.println((i + 1) + " - " + livros.get(i).toString());
 			}
 		}
+	
+	public void AddUsuario(Usuario usuario) {
+		this.usuarios.add(usuario);
+		System.out.println("Cadastro Completo");
+	
 	}
 	
+	public void ListarUsuarioss() {
+		if(usuarios.isEmpty()) {
+			System.out.println("Usuario nao encontrado");
+		}else {
+			System.out.println("lista de usuarios");
+			System.out.println("Total: " + usuarios.size() + " usuário(s)\n");
+			for(int i = 0; i  < usuarios.size(); i++) {
+				System.out.println(("Usuario-" + (i + 1)) +  "\n" + usuarios.get(i).toString());
+			}
+		}
+		
+	}
+}
+	
+	
+
 	
 
